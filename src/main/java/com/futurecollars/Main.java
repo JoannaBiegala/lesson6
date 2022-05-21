48d63b12af81f674fc0478d0e228fc5394acb057
Joanna Biegala <joannabiegala0511@gmail.com>
2022-05-21 16:37:58 +0200

    Kalkulator z testami

---
+++ src/main/java/com/futurecollars/Main.java
@@ -0,0 +1,72 @@
+package com.futurecollars;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        boolean runApp = true;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Podaj jaką operację chcesz wykonać na 2 liczbach: ");
+            System.out.println(" 1 - dodać; 2 - odjąć; 3 - pomnożyć; 4 - podzielić");
+            System.out.println(" 0 - zakończyć działanie kalkulatora \n");
+            int option = scanner.nextInt();
+            if (option == 0) {
+                runApp = false;
+                System.out.println("Działanie kalkutatora zostało zakończone");
+            } else {
+                System.out.println(" Podaj pierwszą liczbę: ");
+                int nr1 = scanner.nextInt();
+                System.out.println(" Podaj pierwszą liczbę: ");
+                int nr2 = scanner.nextInt();
+                System.out.println("Wynik: " + runCalculator(option, nr1, nr2));
+            }
+        } while (runApp);
+    }
+
+    public static String runCalculator(int option, int nr1, int nr2) {
+
+        switch (option) {
+            case 1:
+                return String.valueOf(add(nr1, nr2));
+            case 2:
+                return String.valueOf(subtract(nr1, nr2));
+
+            case 3:
+                return String.valueOf(multiplication(nr1, nr2));
+
+            case 4:
+                return division(nr1, nr2);
+
+            default:
+                return "Błędnie podana opcja";
+        }
+
+    }
+
+    private static int add(int nr1, int nr2) {
+        return nr1 + nr2;
+    }
+
+    private static int subtract(int nr1, int nr2) {
+        return nr1 - nr2;
+    }
+
+    private static int multiplication(int nr1, int nr2) {
+        return nr1 * nr2;
+
+    }
+
+    private static String division(int nr1, int nr2) {
+        if (nr2 == 0) {
+            return "Dzielenie przez zero jest niewykonalne";
+         } else {
+            return String.valueOf(nr1 / nr2);
+        }
+    }
+}
+
+
+
